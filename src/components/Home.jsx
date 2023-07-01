1911a12e860df0bfc82483e15b21e78cf37d046e
deep27b <deep27b@gmail.com>
2023-07-01 23:05:22 +1000

    edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+ import React from 'react';
 import homeImage from "../assets/home.jpg";
 
 const Home = () => {
   return (
     <div name="home" className="h-screen w-full ">
-        <div className="max-w-full mx-auto  flex flex-col items-center justify-center h-full px-10  md:flex-row">
-            <div className="w-2/3">
+        <div className="max-w-full mx-auto  flex flex-col items-center justify-center h-full px-10  sm:flex-row">
+            <div className="w-2/3 mt-20">
                 <img src={homeImage} alt="services" className="rounded-2xl h-4/5 mx-auto  md:w-full" />
             </div>
             <div className="flex flex-col justify-center ml-8 h-full w-1/2 ">
-                <h2 className="text-4xl sm:text-7xl font-bold text-black"> We Service , Maintain And Safety-Check for Cars, UTES and 4WD </h2>
+                <h2 className="text-4xl md:text-6xl font-bold text-black"> We Service , Maintain And Safety-Check for Cars, UTES and 4WD </h2>
                 <p className="text-black text-2xl py-4 max-w-md font-bold">We offer quality auto repairs backed by years of experience!</p>
                 
 
